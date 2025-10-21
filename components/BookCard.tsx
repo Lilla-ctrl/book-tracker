451e305369d62234fdf9147db62db4e7aca6dd1b
Lilla-ctrl <berenyi.lilu@gmail.com>
2025-10-21 14:25:55 +0200

    API call

--- components/BookCard.tsx
+++ components/BookCard.tsx
@@ -1,13 +1,19 @@
-export default function BookCard() {
+interface BookCardProps {
+  title: string;
+  author: string;
+  coverUrl?: string;
+}
+
+export default function BookCard({ title, author, coverUrl}: BookCardProps) {
   return (
     <div className="w-[300px] flex flex-col justify-between text-center bg-red-100 p-3 m-3 rounded-lg shadow-xl">
-      <img
+      {coverUrl && <img
         className="w-60 mx-auto rounded-lg shadow-md"
-        src="https://m.media-amazon.com/images/S/compressed.photo.goodreads.com/books/1700221964i/40275288.jpg"
-        alt=""
-      />
-      <h2 className="text-xl py-1">Samantha Shannon</h2>
-      <h3 className="text-main py-1">The Priory of the Orange Tree</h3>
+        src={coverUrl}
+        alt={`Cover of ${title}`}
+      />}
+      <h2 className="text-xl py-1">{author}</h2>
+      <h3 className="text-main py-1">{title}</h3>
       <button className="bg-cyan-700 mt-3 py-2 rounded-lg cursor-pointer">Add to Bookshelf</button>
     </div>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,33 @@
 import BookCard from "../../components/BookCard";
 
-export default function Home() {
+export default async function Home() {
+  const res = await fetch(
+    `https://www.googleapis.com/books/v1/volumes?q=fantasy`
+  );
+  const data = await res.json();
+  const books = data.items || [];
+  const cleanedBooks = books.map((item: any) => {
+    const volume = item.volumeInfo;
+    return {
+      title: volume.title || "No title",
+      author: volume.authors ? volume.authors[0] : "Unknown author",
+      coverUrl: volume.imgLinks?.thumbnail || "",
+    };
+  });
+
   return (
     <div>
       <main>
         <h1>My Book App</h1>
         <div>
-          <BookCard />
+          {cleanedBooks.map((book, index) => (
+            <BookCard
+              key={index}
+              title={book.title}
+              author={book.author}
+              coverUrl={book.coverUrl}
+            />
+          ))}
         </div>
       </main>
     </div>
